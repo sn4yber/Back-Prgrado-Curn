0a06e1c66d45cd06b7095b553c3e4a6fa532126e
Snayber Madrid <smadridi21@campusuninunez.edu.co>
2026-04-18 09:00:04 +0000

    fix: agregar campo Content al dominio, request y repositorio de posts

---
+++ schema_plain.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict gGq4smoc7yS5SvYKrdczS4hs8GzBhje1i4NCZXIDrFMNLJMP6YjyG1FzKP4A2em
+
+-- Dumped from database version 16.13
+-- Dumped by pg_dump version 16.13
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict gGq4smoc7yS5SvYKrdczS4hs8GzBhje1i4NCZXIDrFMNLJMP6YjyG1FzKP4A2em
+
